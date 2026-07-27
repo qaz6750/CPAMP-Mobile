9fa5d415e3941e4a50e969d6b04a408146278be1
XiaoYeZi <qaz6750@outlook.com>
2026-07-28 00:31:27 +0800

    fix(build): resolve KSP plugin from Maven Central

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,11 @@
 pluginManagement {
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id == "com.google.devtools.ksp") {
+                useModule("com.google.devtools.ksp:symbol-processing-gradle-plugin:${requested.version}")
+            }
+        }
+    }
     repositories {
         maven("https://maven.aliyun.com/repository/google")
         maven("https://maven.aliyun.com/repository/central")
