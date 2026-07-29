dc2c53967cd7582cfc5b26ee5129885bc46ca572
XiaoYeZi <qaz6750@outlook.com>
2026-07-29 18:44:00 +0800

    chore(release): prepare version 1.2.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.cpamp.mobile"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.2.6"
+        versionCode = 11
+        versionName = "1.2.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
