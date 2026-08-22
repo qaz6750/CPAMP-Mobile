79b739d0a082a03cc1d7fe2bf46d86a06fd48762
XiaoYeZi <qaz6750@outlook.com>
2026-08-23 01:44:03 +0800

    chore: prepare v1.4.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.cpamp.mobile"
         minSdk = 26
         targetSdk = 36
-        versionCode = 33
-        versionName = "1.4.7"
+        versionCode = 34
+        versionName = "1.4.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
